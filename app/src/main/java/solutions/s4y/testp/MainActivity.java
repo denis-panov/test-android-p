d6fdc2eea179aa3d038ad3fd4ad7cd58a18eb7d9
Sergey Dolin <v-sedoli@microsoft.com>
2018-08-09 11:51:51 +0500

    migrate to AndroidP release

--- app/src/main/java/solutions/s4y/testp/MainActivity.java
+++ app/src/main/java/solutions/s4y/testp/MainActivity.java
@@ -2,6 +2,9 @@ package solutions.s4y.testp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCenter.start(getApplication(), "4fa91204-c96a-4c62-bbd0-5cff6c34e86b",
+                Analytics.class, Crashes.class);
     }
 }
